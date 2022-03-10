1dd71dc5e20ec242954b36170c0c161ff47a7d91
Blaze Rowland <blaze.rowland@infocss.com>
2022-03-10 16:41:45 -0600

    Wip, want to find a more elgant forms approach, but taking some patience

--- server/src/clientapp/src/modules/product/AddProduct.component.tsx
+++ server/src/clientapp/src/modules/product/AddProduct.component.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 interface AddProductProps {}
 
 export const AddProduct: React.FC<AddProductProps> = () => {
+  const [name, setName] = useState<string | undefined>();
+  const [price, setPrice] = useState<number | undefined>();
+  const [image, setImage] = useState<string | undefined>();
+  const [isPromo, setIsPromo] = useState<boolean | undefined>();
+
   const onSubmit = async (e: any) => {
     e.preventDefault();
+    const product = {
+      name,
+      price,
+      image,
+      isPromo,
+    };
+    console.log(product);
   };
 
   return (
     <form onSubmit={onSubmit}>
+      <input type="text" onChange={(e) => setName(e.target.value)} />
+      <input
+        type="number"
+        onChange={(e) => setPrice(parseInt(e.target.value))}
+      />
+      <input type="text" onChange={(e) => setImage(e.target.value)} />
+      <input type="checkbox" onChange={(e) => setIsPromo(e.target.checked)} />
       <input type="submit" />
     </form>
   );
